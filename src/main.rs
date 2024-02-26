20e9255b3110cd65fe402935228901aa5d65b955
Chance Addis <chance.h.addis@gmail.com>
2024-02-25 20:41:39 -0800

    did some refactoring, specifically, the display function is so much better now. Less writes and more readable.

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,18 @@ pub const BACKTRACK_TIME_STEP_MS: u32 = 100;
 
 fn main() {
     let sudoku_board: [u8; 81] = [
-        0, 3, 0,   9, 8, 4,   2, 0, 0,
-        0, 0, 9,   0, 0, 7,   0, 0, 3,
-        8, 0, 0,   0, 0, 0,   9, 0, 0,
-
-        0, 0, 6,   0, 0, 2,   0, 1, 0,
-        2, 0, 0,   7, 0, 5,   6, 3, 0,
-        0, 0, 0,   0, 9, 0,   4, 0, 8,
-
-        0, 6, 2,   0, 0, 0,   5, 0, 0,
-        1, 0, 0,   0, 0, 0,   0, 7, 0,
-        3, 0, 4,   0, 6, 1,   0, 0, 0
+        0, 3, 0, 9, 8, 4, 2, 0, 0,
+        0, 0, 9, 0, 0, 7, 0, 0, 3,
+        8, 0, 0, 0, 0, 0, 9, 0, 0,
+        0, 0, 6, 0, 0, 2, 0, 1, 0,
+        2, 0, 0, 7, 0, 5, 6, 3, 0,
+        0, 0, 0, 0, 9, 0, 4, 0, 8,
+        0, 6, 2, 0, 0, 0, 5, 0, 0,
+        1, 0, 0, 0, 0, 0, 0, 7, 0,
+        3, 0, 4, 0, 6, 1, 0, 0, 0
     ];
 
-    let mut sudoku = Sudoku::new(sudoku_board);
-    println!("Initial Board \n{}", sudoku);
-    sudoku.backtrack(false);
-
+    let mut sudoku: Sudoku = Sudoku::new(sudoku_board);
+    println!("initial board is \n{}", sudoku);
+    sudoku.backtrack(true);
 }
--- src/sudoku_backtrack.rs
+++ src/sudoku_backtrack.rs
@@ -4,13 +4,11 @@ use std::fmt;
 use std::{thread, time};
 
 pub struct Sudoku {
-    original_board: [u8; 81],
-    solvable_board: [u8; 81],
-    // for visualization only
-    current_cell: u8,
+    start_board: [u8; 81],
+    solve_board: [u8; 81],
+    cell_pointer: u8,
 }
 
-// this differentiates the kinds of cells that are possible for use in coloring them based on those types.
 enum CellType {
     Empty,
     Fixed,
@@ -20,118 +18,105 @@ enum CellType {
 }
 
 fn get_cell_type(sudoku: &Sudoku, index: u8) -> CellType {
-    // add this first to ensure that it will be caught in all cases
-    if sudoku.current_cell == index {
+    if sudoku.cell_pointer == index {
         return CellType::Check;
-    };
-    if sudoku.get_cell(index) == 0 {
+    }
+    if sudoku.get_solve_cell(index) == 0 {
         return CellType::Empty;
-    };
-    if sudoku.get_original_cell(index) != 0 {
+    }
+    if sudoku.get_start_cell(index) != 0 {
         return CellType::Fixed;
-    };
-    if sudoku.current_cell > index {
+    }
+    if sudoku.cell_pointer > index {
         return CellType::Guess;
-    };
+    }
     return CellType::Error;
 }
 
-fn write_cell_type(f: &mut fmt::Formatter<'_>, sudoku: &Sudoku, index: u8) -> fmt::Result {
-    let cell: &u8 = &sudoku.get_cell(index);
+fn color_cells(sudoku: &Sudoku, index: u8) -> ColoredString {
+    let cell: &u8 = &sudoku.get_solve_cell(index);
     match get_cell_type(sudoku, index) {
-        CellType::Empty => write!(f, "{} ", format!("{:1}", cell).black())?,
-        CellType::Fixed => write!(f, "{} ", format!("{:1}", cell).green())?,
-        CellType::Guess => write!(f, "{} ", format!("{:1}", cell).blue())?,
-        CellType::Check => write!(f, "{} ", format!("{:1}", cell).yellow())?,
-        CellType::Error => write!(f, "{} ", format!("{:1}", cell).red())?,
+        CellType::Empty => format!("{:1}", cell).black(),
+        CellType::Fixed => format!("{:1}", cell).green(),
+        CellType::Guess => format!("{:1}", cell).blue(),
+        CellType::Check => format!("{:1}", cell).yellow(),
+        CellType::Error => format!("{:1}", cell).red(),
     }
-    Ok(())
 }
 
 impl fmt::Display for Sudoku {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        // write top horizontal bar
         write!(f, "{}", "┏━━━━━━━┯━━━━━━━┯━━━━━━━┓ \n".black())?;
         for row in 0..9 {
-            // write first vertical bar | on left
-            write!(f, "{}", "┃ ".black())?;
-            write_cell_type(f, self, Sudoku::to_index(row as u8, 0))?;
-            write_cell_type(f, self, Sudoku::to_index(row as u8, 1))?;
-            write_cell_type(f, self, Sudoku::to_index(row as u8, 2))?;
-            write!(f, "{}", "│ ".black())?;
-            write_cell_type(f, self, Sudoku::to_index(row as u8, 3))?;
-            write_cell_type(f, self, Sudoku::to_index(row as u8, 4))?;
-            write_cell_type(f, self, Sudoku::to_index(row as u8, 5))?;
-            write!(f, "{}", "│ ".black())?;
-            write_cell_type(f, self, Sudoku::to_index(row as u8, 6))?;
-            write_cell_type(f, self, Sudoku::to_index(row as u8, 7))?;
-            write_cell_type(f, self, Sudoku::to_index(row as u8, 8))?;
-            write!(f, "{}", "┃".black())?;
-            write!(f, "\n")?;
-            // write middle horizontal bars
-            if (row + 1) % 3 == 0 && row != 8 {
+            write!(
+                f,
+                "{} {} {} {} {} {} {} {} {} {} {} {} {} \n",
+                "┃".black(),
+                color_cells(self, Sudoku::to_index(row, 0)),
+                color_cells(self, Sudoku::to_index(row, 1)),
+                color_cells(self, Sudoku::to_index(row, 2)),
+                "│".black(),
+                color_cells(self, Sudoku::to_index(row, 3)),
+                color_cells(self, Sudoku::to_index(row, 4)),
+                color_cells(self, Sudoku::to_index(row, 5)),
+                "│".black(),
+                color_cells(self, Sudoku::to_index(row, 6)),
+                color_cells(self, Sudoku::to_index(row, 7)),
+                color_cells(self, Sudoku::to_index(row, 8)),
+                "┃".black(),
+            )?;
+            if (row + 1) % 3 == 0 && row < 8 {
                 write!(f, "{}", "┠───────┼───────┼───────┨ \n".black())?;
             }
         }
-        // write bottom horizontal bar
         write!(f, "{}", "┗━━━━━━━┷━━━━━━━┷━━━━━━━┛ \n".black())?;
-        // reset color theme
         colored::control::unset_override();
         Ok(())
     }
 }
 
-#[allow(dead_code)]
 impl Sudoku {
-    // FUNDAMENTAL OPERATIONS
+    // FUNDAMENTAL FUNCTIONS
 
-    /// initializes a new Sudoku
-    pub fn new(board: [u8; 81]) -> Self {
-        Sudoku {
-            original_board: board,
-            solvable_board: board,
-            current_cell: 0,
+    /// initializes the sudoku
+    pub fn new(sudoku_board: [u8; 81]) -> Self {
+        Self {
+            start_board: sudoku_board,
+            solve_board: sudoku_board,
+            cell_pointer: 0,
         }
     }
-    /// converts an index into its corresponding row and col
-    pub fn to_index(row: u8, col: u8) -> u8 {
+    /// takes a row, col and converts it into the corresponding index
+    fn to_index(row: u8, col: u8) -> u8 {
         col + 9 * row
     }
-    /// converts a row, col pair into its corresponding indeex
-    pub fn to_row_col(index: u8) -> (u8, u8) {
+    /// takes a index and converts it into the corresponding (row, col) pair
+    fn to_row_col(index: u8) -> (u8, u8) {
         (index / 9, index % 9)
     }
-    /// gets the cell in the solvable board at index
-    fn get_cell(&self, index: u8) -> u8 {
-        self.solvable_board[index as usize]
+    /// gets the cell in start board at index -index-
+    fn get_start_cell(&self, index: u8) -> u8 {
+        self.start_board[index as usize]
     }
-    /// gets the cell in the original board at index
-    fn get_original_cell(&self, index: u8) -> u8 {
-        self.original_board[index as usize]
+    /// gets the cell in solve board at index -index-
+    fn get_solve_cell(&self, index: u8) -> u8 {
+        self.solve_board[index as usize]
     }
-    /// set the cell to value in the solvable board at index
-    fn set_cell(&mut self, index: u8, value: u8) {
-        self.solvable_board[index as usize] = value
+    /// sets the cell to value in solve board at index -index-
+    fn set_solve_cell(&mut self, index: u8, value: u8) {
+        self.solve_board[index as usize] = value;
     }
-    /// checks if the index is in the bounadies of the board
-    pub fn is_in_bounds(index: i8) -> bool {
-        if 0 <= index && index < 81 {
+    /// check if the cell_pointer could be increased without overflow
+    fn can_increase_cell_pointer(&mut self) -> bool {
+        if self.cell_pointer < 80 {
             true
         } else {
             false
         }
     }
-    /// check if the current_cell could be increased without overflow
-    fn can_increase_current_cell(&mut self) -> bool {
-        if self.current_cell < 80 {
-            true
-        } else {
-            false
-        }
-    }
-    /// check if the current_cell could be decreased without going underflow
-    fn can_decrease_current_cell(&mut self) -> bool {
-        if 0 < self.current_cell {
+    /// check if the cell_pointer could be decreased without going underflow
+    fn can_decrease_cell_pointer(&mut self) -> bool {
+        if 0 < self.cell_pointer {
             true
         } else {
             false
@@ -140,62 +125,57 @@ impl Sudoku {
 
     // VALIDATION
 
-    /// the function that finds whether a set of cells contains more than 1 of a specific value. this is then used to validate that the addition is a valid sudoku board.
-    fn is_valid_helper(&self, start_cell: u8, cell_pattern: [u8; 9], value: u8) -> bool {
-        let cells_to_check = cell_pattern.iter().map(|x| x + start_cell);
-        let mut count = 0;
-        for cell_index in cells_to_check {
-            if self.solvable_board[cell_index as usize] == value {
-                count += 1;
-            }
-        }
-        if count <= 1 {
-            true
-        } else {
-            false
-        }
+    /// takes start_index, a pattern of cell displacements from start_index, and a value, and checks whether there are at least 2 of that value in the cells in the cell pattern (hence an invalid cell_pattern for that value).
+    pub fn validation_function(&self, start_cell: u8, cells_pattern: [u8; 9], value: u8) -> bool {
+        cells_pattern
+            .iter()
+            .map(|x| x + start_cell)
+            .filter(|x| self.get_solve_cell(*x) == value)
+            .count()
+            < 2
     }
-    /// determines is a the row of index has less than two of value -- hence it is valid for that value.
+    /// performs validation on the row of the index -index- for value -value-
     fn is_valid_row_for_value(&self, index: u8, value: u8) -> bool {
         let (row, _) = Sudoku::to_row_col(index);
         let start_cell = 9 * row;
         let cells_pattern = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-        self.is_valid_helper(start_cell, cells_pattern, value)
+        self.validation_function(start_cell, cells_pattern, value)
     }
-    /// determines is a the col of index has less than two of value -- hence it is valid for that value.
+    /// performs validation on the col of the index -index- for value -value-
     fn is_valid_col_for_value(&self, index: u8, value: u8) -> bool {
         let (_, col) = Sudoku::to_row_col(index);
         let start_cell = col;
         let cells_pattern = [0, 9, 18, 27, 36, 45, 54, 63, 72];
-        self.is_valid_helper(start_cell, cells_pattern, value)
+        self.validation_function(start_cell, cells_pattern, value)
     }
-    /// determines is a the box of index has less than two of value -- hence it is valid for that value.
+    /// performs validation on the box of the index -index- for value -value-
     fn is_valid_box_for_value(&self, index: u8, value: u8) -> bool {
         let (row, col) = Sudoku::to_row_col(index);
         let region = (col / 3) + 3 * (row / 3);
         let start_cell = 3 * (region % 3) + 27 * (region / 3);
         let cells_pattern = [0, 1, 2, 9, 10, 11, 18, 19, 20];
-        self.is_valid_helper(start_cell, cells_pattern, value)
+        self.validation_function(start_cell, cells_pattern, value)
     }
-    /// determines is a the row of index is valid for all numbers 1..9. (note: that this is a slow implemnetation, but it only needs to be used once, so its not particularly important)
+    // (note: these functions are not optimized, but it only needs to be used once, so its not particularly important)
+    /// performs validation on the row of index -index- is valid for all numbers 1..9.
     fn is_valid_row(&self, index: u8) -> bool {
         (1..=9)
             .into_iter()
             .all(|v| self.is_valid_row_for_value(index, v))
     }
-    /// determines is a the col of index is valid for all numbers 1..9. (note: that this is a slow implemnetation, but it only needs to be used once, so its not particularly important)
+    /// performs validation on the col of index -index- is valid for all numbers 1..9.
     fn is_valid_col(&self, index: u8) -> bool {
         (1..=9)
             .into_iter()
             .all(|v| self.is_valid_col_for_value(index, v))
     }
-    /// determines is a the col of index is valid for all numbers 1..9. (note: that this is a slow implemnetation, but it only needs to be used once, so its not particularly important)
+    /// performs validation on the box of index -index- is valid for all numbers 1..9.
     fn is_valid_box(&self, index: u8) -> bool {
         (1..=9)
             .into_iter()
             .all(|v| self.is_valid_box_for_value(index, v))
     }
-    /// validates whether the current solvable board is valid
+    /// performs validatation on the entire solvable board
     fn is_valid_board(&self) -> bool {
         for row in 0..9 {
             if !self.is_valid_row(9 * row) {
@@ -219,32 +199,29 @@ impl Sudoku {
 
     /// determines whether a specific value placed at some index will yeild a valid Sudoku board (implicitly assumes that the current board is already valid)
     fn is_valid_placement(&mut self, index: u8, value: u8) -> bool {
-        let value_at_cell = self.get_cell(index);
-        self.set_cell(index, value);
+        let value_at_cell = self.get_solve_cell(index);
+        self.set_solve_cell(index, value);
         if self.is_valid_row(index) && self.is_valid_col(index) && self.is_valid_box(index) {
-            self.set_cell(index, value_at_cell);
+            self.set_solve_cell(index, value_at_cell);
             return true;
         }
-        self.set_cell(index, value_at_cell);
+        self.set_solve_cell(index, value_at_cell);
         false
     }
     /// finds the next higest valid vlaue to place in some index, gives 0 if there are no more valid placements (implicitly assumes that the current board is already valid)
     fn get_next_valid_value_from_cell(&mut self, index: u8) -> u8 {
-        let current_value = self.get_cell(index);
+        let current_value = self.get_solve_cell(index);
         for i in (current_value + 1)..=9 {
             if self.is_valid_placement(index, i) {
                 return i;
             }
         }
-        return 0; // 0 means that are no more valid values
+        0 // 0 means that are no more valid values
     }
-
+    /// for a given iteration, determines (based on arbitrary criteria) whether that iteration should be visualized. this speeds of the visualization and makes it more interesting. can easily be turned off just returning true
     fn should_visualize_iteration(&mut self) -> bool {
-        let next_valid_value = self.get_next_valid_value_from_cell(self.current_cell);
-        let is_empty = match get_cell_type(self, self.current_cell) {
-            CellType::Empty => true,
-           _ => false
-        };
+        let next_valid_value = self.get_next_valid_value_from_cell(self.cell_pointer);
+        let is_empty = true; // alternatively: let is_empty = self.get_solve_cell(self.cell_pointer) == 0;
         if next_valid_value > 0 && !is_empty {
             true
         } else {
@@ -257,29 +234,29 @@ impl Sudoku {
             println!("board has a contradiction");
             return false;
         }
-        while self.current_cell < 81 {
-            let next_valid_value = self.get_next_valid_value_from_cell(self.current_cell);
+        while self.cell_pointer < 81 {
+            let next_valid_value = self.get_next_valid_value_from_cell(self.cell_pointer);
             if next_valid_value > 0 {
-                self.set_cell(self.current_cell, next_valid_value);
-                match self.can_increase_current_cell() {
-                    true => self.current_cell += 1,
+                self.set_solve_cell(self.cell_pointer, next_valid_value);
+                match self.can_increase_cell_pointer() {
+                    true => self.cell_pointer += 1,
                     false => break,
                 }
-                while self.get_original_cell(self.current_cell) != 0 {
-                    match self.can_increase_current_cell() {
-                        true => self.current_cell += 1,
+                while self.get_start_cell(self.cell_pointer) != 0 {
+                    match self.can_increase_cell_pointer() {
+                        true => self.cell_pointer += 1,
                         false => break,
                     }
                 }
             } else {
-                self.set_cell(self.current_cell, 0);
-                match self.can_decrease_current_cell() {
-                    true => self.current_cell -= 1,
+                self.set_solve_cell(self.cell_pointer, 0);
+                match self.can_decrease_cell_pointer() {
+                    true => self.cell_pointer -= 1,
                     false => break,
                 }
-                while self.get_original_cell(self.current_cell) != 0 {
-                    match self.can_decrease_current_cell() {
-                        true => self.current_cell -= 1,
+                while self.get_start_cell(self.cell_pointer) != 0 {
+                    match self.can_decrease_cell_pointer() {
+                        true => self.cell_pointer -= 1,
                         false => break,
                     }
                 }
@@ -293,14 +270,14 @@ impl Sudoku {
             }
         }
         // terminal state is reached
-        if self.current_cell == 80 {
-            self.current_cell += 1; // do this for the visualization only
+        if self.cell_pointer == 80 {
+            self.cell_pointer += 1; // do this for the visualization only
             assert!(self.is_valid_board());
             println!("Board Solved!");
             println!("{}", self);
             true // true for solvable board
-        } else if self.current_cell == 0 {
-            self.current_cell = 81; // do this for the visualization only
+        } else if self.cell_pointer == 0 {
+            self.cell_pointer = 81; // do this for the visualization only
             println!("Board is impossible");
             println!("{}", self);
             false // false for unsolvable board
